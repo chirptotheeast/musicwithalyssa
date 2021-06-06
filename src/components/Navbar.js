5388fb37de11a56c722381f24fedd57542105899
chirptotheeast <git@github.com>
2021-06-06 19:02:32 -0400

    update

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Welcome extends Component {
+    render() {
+        return (
+            <div>
+                navbar layout will go here 
+            </div>
+        )
+    }
+}
