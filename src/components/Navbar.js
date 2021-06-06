24a71df4111fbe6a23aec64c1ffd04ff2cbf78ba
chirptotheeast <alyssa.easterly@yahoo.com>
2021-06-06 19:04:41 -0400

    push change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,8 @@ export default class Welcome extends Component {
         return (
             <div>
                 navbar layout will go here 
+
+                <h1> tell me about how play the harmonica</h1>
             </div>
         )
     }
